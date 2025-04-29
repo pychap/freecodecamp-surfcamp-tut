204e5ef00e248709de8ae9af470e3c50139ca828
Peter Chapman <pychapman22@gmail.com>
2025-04-29 10:03:32 -0700

    Hero component display, home page.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,4 @@
+import { HeroSection } from "@/components/blocks/HeroSection";
 import { getHomePage } from "@/data/loaders";
 import { notFound } from "next/navigation";
 
@@ -13,11 +14,11 @@ async function loader() {
 // we then consume the data
 export default async function HomeRoute() {
   const data = await loader();
+  const blocks = data?.blocks || [];
   console.log(data);
   return (
     <div>
-      <h1>{data.title}</h1>
-      <p>{data.description}</p>
+      <HeroSection {...blocks[0]} />
     </div>
   );
 }
